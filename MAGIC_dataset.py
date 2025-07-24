1700c3295036c5a044be60b6f8fb083d5d0404a8
VS-Kosher <itorousa@gmail.com>
2025-07-24 10:59:05 +0100

    Added MAGIC dataset processing and visualization script

---
+++ MAGIC_dataset.py
@@ -0,0 +1,19 @@
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+
+
+cols = ["fLength", "fWidth", "fSize", "fConc", "fConc1", "fAsym", "fM3Long", "fM3Trans", "fAlpha", "fDist", "class"]
+df = pd.read_csv("C:\\Code\\Code\\.Files\\magic04.data", names=cols)
+df["class"] = (df["class"] == 'g').astype(int)
+
+for label in cols[:-1]:
+    plt.hist(df[df["class"] == 1][label], color='blue', label='gamma', alpha=0.7, density=True)
+    plt.hist(df[df["class"] == 0][label], color='red', label='hadron', alpha=0.7, density=True)
+    plt.title(label)
+    plt.ylabel("Probability")
+    plt.xlabel(label)
+    plt.legend()
+    plt.show()
+
+train, valid, test = np.split(df.sample(frac=1), [int(0.6*len(df)), int(0.8*len(df))])

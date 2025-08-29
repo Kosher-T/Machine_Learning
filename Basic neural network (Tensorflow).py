4ac346e23881657cdb3a6240ba18e926a34df6f0
VS-Kosher <itorousa@gmail.com>
2025-08-29 23:48:27 +0100

    Add loss and accuracy plotting functions for neural network training

--- Basic neural network (Tensorflow).py
+++ Basic neural network (Tensorflow).py
@@ -88,3 +88,41 @@ test, X_test, y_test = scale_dataset(test, oversample=False)
 
 
 
+import tensorflow as tf
+
+def plot_loss(history):
+    plt.plot(history.history['loss' ], label='loss')
+    plt.plot(history.history['val_loss'], label='val_loss')
+    plt.xlabel('Epoch')
+    plt.ylabel('Binary crossentropy')
+    plt.legend()
+    plt.grid(True)
+    plt.show()
+
+def plot_accuracy(history):
+    plt.plot(history.history['accuracy'], label='accuracy')
+    plt.plot(history.history['val_accuracy'], label='val_accuracy')
+    plt.xlabel('Epoch')
+    plt.ylabel('Accuracy')
+    plt.legend( )
+    plt.grid(True)
+    plt.show()
+
+nn_model = tf.keras.Sequential([
+    tf.keras.layers.Dense(32, activation='relu'),
+    tf.keras.layers.Dense(32, activation='relu'),
+    tf.keras.layers.Dense(1, activation='sigmoid')
+])
+
+nn_model.compile(optimizer='adam',
+              loss='binary_crossentropy',
+              metrics=['accuracy'])
+
+nn_model.fit(X_train, y_train, epochs=10)
+
+history = nn_model.fit(
+    X_train, y_train, epochs=100, batch_size=32, validation_split=0.2, verbose=0
+    )
+
+plot_loss(history)
+plot_accuracy(history)
